a836b392b8f37c90c51975ef9d0f8bf4351040e0
bvekv1 <bvekp1@gmail.com>
2020-01-18 11:45:55 +0545

    user registe

--- database/dbConnection.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(() => console.log("Mongodb connected"))
-.catch(err => console.log(err));
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-const app = express();
+const dotenv = require('dotenv').config();
 
+var morgan = require('morgan');
+
+const userController = require('./controllers/userController');
+
+
+
+const app = express();
+app.use(morgan('tiny'));
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.post('/registerUser',userController.register);
+
 
 mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 .then(() => console.log("Mongodb connected"))
 .catch(err => console.log(err));
 
-    app.listen(process.env.PORT, () => {
+ app.listen(process.env.PORT, () => {
         console.log(`App is running at localhost:${process.env.PORT}`);
     });
